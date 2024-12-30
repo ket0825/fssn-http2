7b180fa188b069d81b455e200f6e3d31cc84a11d
etkim <ket0825@gmail.com>
2024-12-30 21:35:45 +0900

    chore: 주석 처리 해제

--- http2-server-evolve/server.py
+++ http2-server-evolve/server.py
@@ -24,33 +24,33 @@ async def read_root(request: Request) -> HTMLResponse:
     print(f"Got connection: {protocol}/{http_version} from {client_addr}")
     return HTMLResponse(content=f"Hello")
 
-# if __name__ == "__main__":
-#     import hypercorn
-#     import hypercorn.asyncio
-#     config = hypercorn.Config()
-#     config.bind = ["localhost:443", "localhost:80"]    
+if __name__ == "__main__":
+    import hypercorn
+    import hypercorn.asyncio
+    config = hypercorn.Config()
+    config.bind = ["localhost:443", "localhost:80"]    
     
-#     config.certfile = "./http2-server/cert.pem"
-#     config.keyfile = "./http2-server/key.pem"    
-#     config.alpn_protocols = ["h2", "http/1.1"]
+    config.certfile = "./http2-server/cert.pem"
+    config.keyfile = "./http2-server/key.pem"    
+    config.alpn_protocols = ["h2", "http/1.1"]
     
-#     if config.ssl_enabled:
-#         print(f"Running on https://localhost:443 (CTRL + C to quit)")
-#     else:
-#         print(f"Running on http://localhost:80 (CTRL + C to quit)")                        
+    if config.ssl_enabled:
+        print(f"Running on https://localhost:443 (CTRL + C to quit)")
+    else:
+        print(f"Running on http://localhost:80 (CTRL + C to quit)")                        
 
-#     if "h2" in config.alpn_protocols:
-#         print("HTTP/2 is enabled")
-#     elif "http/1.1" in config.alpn_protocols:
-#         print("HTTP/1.1 is enabled")
-#     else:
-#         print("HTTP/2 and HTTP/1.1 are disabled")
+    if "h2" in config.alpn_protocols:
+        print("HTTP/2 is enabled")
+    elif "http/1.1" in config.alpn_protocols:
+        print("HTTP/1.1 is enabled")
+    else:
+        print("HTTP/2 and HTTP/1.1 are disabled")
         
-#     if not check_tls(config.certfile, config.keyfile):
-#         raise("TLS configuration error")        
+    if not check_tls(config.certfile, config.keyfile):
+        raise("TLS configuration error")        
     
-#     import asyncio
-#     asyncio.run(hypercorn.asyncio.serve(app, config))
+    import asyncio
+    asyncio.run(hypercorn.asyncio.serve(app, config))
 
 # MARK: CMD below
 # poetry run hypercorn --certfile ./http2-server/cert.pem --keyfile ./http2-server/key.pem --bind localhost:8000 ./http2-server-evolve/server.py:app
